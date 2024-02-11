b0e7d6c5f4cd11446a34ee198d9b7f6e57646a9e
unknown <Mr_Buzik@mail.ru>
2024-02-11 20:12:42 +0500

    Фиксы деплоя

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router}  from "react-router-dom";
 import App from './App';
 
+const basename = '/sapper';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={basename}>
       <App />
     </Router>
   </React.StrictMode>
